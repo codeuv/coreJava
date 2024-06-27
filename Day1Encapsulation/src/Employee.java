4ae19328473f8373fc6e187595e8b6f2663c1481
codeuv <www.yuvarjdevil@gmail.com>
2024-06-27 18:47:22 +0530

    Employee class added

--- Day1Encapsulation/src/Employee.java
+++ Day1Encapsulation/src/Employee.java
@@ -1,4 +1,28 @@
 public class Employee {
-     int id = 0 ;
-     String name = "";
+     private int id = 0 ;
+     private String name = "";
+
+     public void setId(int id) {
+          this.id = id;
+     }
+
+     public void setName(String name) {
+          this.name = name;
+     }
+
+     public int getId() {
+          return id;
+     }
+
+     public String getName() {
+          return name;
+     }
+
+     @Override
+     public String toString() {
+          return "Employee{" +
+                  "id=" + id +
+                  ", name='" + name + '\'' +
+                  '}';
+     }
 }
--- Day1Encapsulation/src/Main.java
+++ Day1Encapsulation/src/Main.java
@@ -2,8 +2,23 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        //Demonstaration of Encapsulation
+        //Encapsulation : concept of OOPS , bundle or group the attributes and behaviour into a single class
+        //Attributes - variable / data members
+        //Behaviour - methods or function
         Employee e = new Employee();
-        System.out.println(e.id);
-        System.out.println(e.name);
+
+        System.out.println("Before");
+        System.out.println(e.getId());
+        System.out.println(e.getName());
+        System.out.println(e.toString());
+        System.out.println("Setting the attributes for it ...");
+        e.setId(333);
+        e.setName("Chris Gayle");
+        System.out.println("After");
+        System.out.println(e.getId());
+        System.out.println(e.getName());
+        System.out.println(e.toString());
+
     }
 }
