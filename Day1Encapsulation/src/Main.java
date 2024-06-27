7f9052646a6b1365edbb9751df26b343e37a946c
codeuv <www.yuvarjdevil@gmail.com>
2024-06-27 18:32:15 +0530

    Employee class added

---
+++ Day1Encapsulation/src/Main.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Employee e = new Employee();
+        System.out.println(e.id);
+        System.out.println(e.name);
+    }
+}
